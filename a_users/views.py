5fb6231be8249707f9b6aecc2a4c618d8a3a0981
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-16 00:33:03 +0300

    👍 Adding the tabs functionality to the profile page.

--- a_users/views.py
+++ a_users/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib import messages
-from django.http import Http404
+from django.http import Http404, HttpResponse
+from django.db.models import Count
 from .forms import ProfileForm
+from a_posts.models import Post
+from a_posts.forms import ReplyCreateForm
 
 
 @login_required
@@ -12,12 +16,49 @@ def profile_view(request, username=None):
     # Fetch the profile for the provided username or the current user
     user = get_object_or_404(User, username=username) if username else request.user
     profile = getattr(user, "profile", None)
+    posts = profile.user.posts.all()
 
     if not profile:
         raise Http404("Profile not found.")
 
-    context = {"profile": profile}
+    if request.htmx:
+        if "top-posts" in request.GET:
+            # Filter posts with at least one like
+            posts = (
+                profile.user.posts.annotate(num_likes=Count("likes"))
+                .filter(num_likes__gt=0)
+                .order_by("-num_likes")
+            )
+
+        elif "top-comments" in request.GET:
+            # Filter comments with at least one like
+            comments = (
+                profile.user.comments.annotate(num_likes=Count("likes"))
+                .filter(num_likes__gt=0)
+                .order_by("-num_likes")
+            )
+            reply_form = ReplyCreateForm()
+            context = {
+                "comments": comments,
+                "form": reply_form,
+            }
+            return render(
+                request, "snippets/loop_profile_comments.html", context, 
+            )
+        elif "liked-posts" in request.GET:
+            # Fetch posts liked by the user
+            posts = Post.objects.filter(likes=request.user).order_by(
+                "-likedpost__created_at"
+            )
+        return render(request, "snippets/loop_profile_posts.html", {"posts": posts})
+            
+
+    # Fallback for both HTMX and non-HTMX full profile views
+    context = {"profile": profile, "posts": posts}
+    
+    # Return full profile view for non-HTMX or regular requests
     return render(request, "a_users/profile.html", context)
+
 
 
 @login_required
