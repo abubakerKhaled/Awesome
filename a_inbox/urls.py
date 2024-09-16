4b42d43b81da7ca780604a938ad036aa9efffcf4
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2024-09-16 18:44:39 +0300

    Adding the conversation url and view.

--- a_inbox/urls.py
+++ a_inbox/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 app_name = "a_inbox"
 
-urlpatterns = [
-    path('', inbox_view, name='inbox')
+urlpatterns = [ 
+    path('', inbox_view, name='inbox'),
+    path('conversation/<uuid:conversation_id>', inbox_view, name='inbox')
 ]
 
